bec67087f18d267f1368f67e5f7aa205cb7a0f60
Kevin <floreskevin51398@gmail.com>
2023-04-23 21:52:43 -0600

    skeleton content for the website need to fix it and the layout + make nave bar+header banner - KevFlo

---
+++ pages/Join.js
@@ -0,0 +1,40 @@
+
+
+export default function Join() {
+    return (
+        <>
+            You can join our society in person at the UH Observatory on most Friday’s from 11 AM – 4 PM . Contact our Treasurer at asuh@nsm.uh.edu to arrange a specific time to meet or online via PayPal or Venmo. The fee for the 2020/2021 academic year is $20. This will cover membership through August 23rd, 2021 (which includes all of our summer activities). You may also join for a semester for $15. 
+
+            Members of ASUH receive some extraordinary benefits including, but not limited to:
+
+            •	Access to all levels of our Astronomy Workshops. 
+            •	Learn to use professional astronomer equipment including software, high power telescopes and binoculars, cameras and other tools of the trade.
+            •	Private invitation to viewing events in the Observatory and at other dark sites. 
+            •	Learning from experienced amateur and professional astronomers. 
+
+
+            ## Join ASUH
+
+
+            ***
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="CY6R5XQ35WXZ2">
+            <table>
+            <tr><td><input type="hidden" name="on0" value="ASUH Membership Dues 2020-2021">ASUH Membership Dues Fall 2020-2021</td></tr><tr><td><select name="os0">
+            <option value="Year">Year $21 USD</option>
+            <option value="Semester">Semester $15.76 USD</option>
+            </select> </td></tr>
+            </table>
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+            </form>
+
+            ***
+            Our Venmo is @AstronomySociety
+
+            ***
+        </>
+    )
+}
---
+++ pages/Social.js
@@ -0,0 +1,43 @@
+
+export default function Socials() {
+    return (
+        <>
+            <h1>Keep up with the Astronomy Society by following us on Social Media!</h1>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9 col-sm-push-4 col-md-push-3 main">
+                        <main>
+                        <section>
+                            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+            fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+            <div class="fb-page" data-href="https://www.facebook.com/AstroSocietyUH/" data-tabs="timeline" data-width="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AstroSocietyUH/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AstroSocietyUH/">Astronomy Society at the University of Houston - Main Campus</a></blockquote></div>			  </section>
+                        </main>
+                    </div>
+                    <div class="col-sm-4 col-md-3 col-sm-pull-8 col-md-pull-9 sidebar">
+                        <small>
+                            
+
+
+            <a class="twitter-timeline" href="https://twitter.com/AstroSocietyUH" data-width="350" data-height="600">Tweets by Astronomy Soceity at UH </a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-2"></script>
+
+            <a href="https://www.instagram.com/astrosocietyuh/"><img src="/content/8-asuh-social-media/insta-title.png" alt=""></a>
+
+            <!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/435b423ec49757cd8b22c4de59ed9c3d.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+
+            <a href="asuh-social-media/snapchat"><img src="http://asuh.phys.uh.edu/content/9-asuh-social-media/snapcode.jpg" alt="Snapcode" class="right"><style>
+            img {
+                display: block;
+                margin-left: 0.5;
+                margin-right: auto;
+            }
+            </style></a>
+        </>
+    )
+}
