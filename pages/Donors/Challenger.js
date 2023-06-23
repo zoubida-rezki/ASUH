9f479d267079dcb96b37c56569a426e7c126aa6e
Kevin Flores <floreskevin51398@gmail.com>
2023-06-23 18:38:55 -0500

    Created Donors pages -Kevin

---
+++ pages/Donors/Challenger.js
@@ -0,0 +1,22 @@
+
+export default function donors2018 () {
+
+    return(
+        <>
+            <h1 className='pd-16 pl-16 text-xl '>Donors</h1>
+            <h2>2017-2018 Donors</h2>
+
+            <h1 className='text-3xl'>2017-2018 The Challenger Division Donors</h1>
+
+            <ul className='grid grid-rows-5  grid-col-1 gap-4  list-none pd-16 pl-16 '>
+                <li>
+                    <div>
+                        <h2 className="text-xl">Earth Level Donors($500+)</h2>
+                        <p>Susan Street     Land, Sea and Sky </p>
+                    </div>
+                </li>
+            </ul>
+        
+        </>
+    )
+}
